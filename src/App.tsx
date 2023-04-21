49af094c3a0fd61ca0425ed6f0f78837fcbe7e96
Vysakhan H <vysakhanh@firstsense.tech>
2023-04-21 20:57:59 +0530

    added loading prompt. removed tauri icons

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ interface Network {
   id: number;
 }
 
-interface IP{
+interface IP {
   name: string;
   id: number;
 }
@@ -16,6 +16,7 @@ function App() {
   const [Networks, setNetworks] = useState(Array<Network>());
   const [IPs, setIPs] = useState(Array<IP>());
   const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false);
 
   async function check_network_interfaces() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -23,15 +24,19 @@ function App() {
     setNetworks(await invoke<Array<Network>>("list_network_interfaces"));
   }
 
-  async function list_ips(interface_name: String){
+  async function list_ips(interface_name: String) {
     // console.log(await invoke<Array<IP>>("list_ips",{interfaceName: interface_name}));
-    setIPs(await invoke<Array<IP>>("list_ips",{interfaceName: interface_name}));
+    setLoading(true);
+    setIPs(
+      await invoke<Array<IP>>("list_ips", { interfaceName: interface_name })
+    );
+    setLoading(false);
   }
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
+      <h1>IP scanner</h1>
+      {/*
       <div className="row">
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo vite" alt="Vite logo" />
@@ -44,7 +49,7 @@ function App() {
         </a>
       </div>
 
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+      <p>Click on the Tauri, Vite, and React logos to learn more.</p>*/}
 
       <div className="row">
         <form
@@ -53,11 +58,6 @@ function App() {
             check_network_interfaces();
           }}
         >
-          {/*<input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />*/}
           <button type="button">Find network interfaces</button>
         </form>
       </div>
@@ -69,15 +69,35 @@ function App() {
             list_ips((e.target as HTMLInputElement).value);
           }}
         >
-        {Networks.map(network => 
-          <button type="button" key={network.id} value={network.name.toString()}>{network.name}</button>)
-        }
+          {Networks.map((network) => (
+            <button
+              type="button"
+              key={network.id}
+              value={network.name.toString()}
+            >
+              {network.name}
+            </button>
+          ))}
         </form>
       </div>
       <div>
-        {IPs.map(ip =>  
-         <button type="button" key={ip.id} value={ip.name.toString()}>{ip.name}</button>)
-        }
+        {loading ? (
+          <div>Loading. Please Wait.</div>
+        ) : (
+          <div>
+            {IPs.length == 0 ? (
+              <div>nothing found</div>
+            ) : (
+              <div>
+                {IPs.map((ip) => (
+                  <button type="button" key={ip.id} value={ip.name.toString()}>
+                    {ip.name}
+                  </button>
+                ))}
+              </div>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
